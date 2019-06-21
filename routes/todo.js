22d0da107eb5a3b8116afdd578532f648b2b5d66
Carlo <carlomjavier@gmail.com>
2019-06-21 01:02:04 +0000

    attempt to isolate table to specific userID

--- routes/todo.js
+++ routes/todo.js
@@ -6,14 +6,25 @@ const router  = express.Router();
 
 module.exports = (knex) => {
 
+    // router.get("/", (req, res) => {
+    //   knex    
+    //     .select("*")
+    //     .from("todo")
+    //     .then((results) => {
+    //         res.json(results);
+    //     });
+    
+    // });
     router.get("/", (req, res) => {
-      knex    
+        console.log("USERID: ", req.cookie('userID', results[0]));
+        knex
         .select("*")
-        .from("todo")
-        .then((results) => {
-            res.json(results);
-        });
-    });
+          .from("todo")
+          .where("userID")
+          .where("movie")
+          res.cookie('userID', results[0])
+          res.redirect("/");
 
+    });
     return router;
 }
